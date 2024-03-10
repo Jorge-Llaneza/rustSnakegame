76881401f003806f2a0850f38b8004a5570c14e4
Jorge-Llaneza <llanezarodriguezjorge@gmail.com>
2024-03-10 14:42:27 +0100

    fixed incorrect syntax

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,45 @@
+
+
 const GAME_X: usize = 20;
 const GAME_Y: usize = 20;
 
-const DEFAULT_CELL: Cell = {
-    remaining_snake_frames = 0;
-    has_food = false;
+const DEFAULT_CELL: Cell = Cell {
+    remaining_snake_frames : 0,
+    has_food : false,
+    
 };
 
 fn main() {
-
+    
     
 }
 
-struct Grid<T> {
-    grid: [[T ; GAME_X]; GAME_X],
+struct Grid {
+    grid: [[Cell ; GAME_X]; GAME_X],
     
 }
 
 struct Cell {
     remaining_snake_frames: i32 ,
     has_food: bool ,
-    is_snake_head: bool,
+   
 }
 
-impl Grid<T> {
+impl Grid {
 
-    fn new() -> Grid<Cell> {
-        for
+    fn new() -> Grid {
+        let mut grid: Grid;
 
+        let grid_height = grid.grid.len();
+        let grid_width = grid.grid[0].len();
+
+       
+        for row_index in ..grid_height {
+            for cell_index in ..grid_width {
+                grid.grid[grid_height][grid_width] = DEFAULT_CELL;
+            }
+        }
+        grid
     }
 
 }
--- target/debug/deps/snake_game_1-470c98724d482a77.d
+++ target/debug/deps/snake_game_1-470c98724d482a77.d
@@ -1,5 +1,5 @@
-/home/jorge/Desktop/rust/snake_game_1/target/debug/deps/snake_game_1-470c98724d482a77: src/main.rs
+/home/jorge/Desktop/rust/rustSnakegame/target/debug/deps/snake_game_1-470c98724d482a77: src/main.rs
 
-/home/jorge/Desktop/rust/snake_game_1/target/debug/deps/snake_game_1-470c98724d482a77.d: src/main.rs
+/home/jorge/Desktop/rust/rustSnakegame/target/debug/deps/snake_game_1-470c98724d482a77.d: src/main.rs
 
 src/main.rs:
